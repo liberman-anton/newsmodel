5b3897a0122c80041f3e8238f196fcaf986d9c68
liberman-anton <antonliberman@gmail.com>
2015-07-23 10:38:25 +0600

    +IModel +View

---
+++ www/classes/View.php
@@ -0,0 +1,61 @@
+<?php
+
+class View
+    implements Iterator
+{
+    protected $data = [];
+
+    public function __set($k, $v)
+    {
+        $this->data[$k] = $v;
+    }
+
+    public function __get($k)
+    {
+        return $this->data[$k];
+    }
+
+    public function render($template)
+    {
+        foreach ($this->data as $key => $val) {
+            $$key = $val;
+        }
+
+        ob_start();
+        include __DIR__ . '/../views/' . $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+
+    public function display($template)
+    {
+        echo $this->render($template);
+    }
+
+    public function current()
+    {
+
+    }
+
+    public function next()
+    {
+
+    }
+
+    public function key()
+    {
+
+    }
+
+    public function valid()
+    {
+
+    }
+
+    public function rewind()
+    {
+
+    }
+}
